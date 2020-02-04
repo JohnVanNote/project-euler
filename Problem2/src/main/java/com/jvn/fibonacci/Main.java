8dadfd34e3da382f6faef57b025672f08cdb9b95
John Van Note <johnlvannote@protonmail.com>
2020-02-03 22:33:27 -0500

    Built the main method

--- Problem2/src/main/java/com/jvn/fibonacci/Main.java
+++ Problem2/src/main/java/com/jvn/fibonacci/Main.java
@@ -5,54 +5,15 @@ import java.util.List;
 
 public class Main {
 
-  private static final int INIT_SENTINEL = -1;
   private static final int MAX = 4000000;
-  private static final int[] MEMOIZED = init(MAX+1);
 
-  private static int[] init(int size) {
-    int[] init = new int[size];
-    for (int i=0; i<size; i++) {
-      init[i] = INIT_SENTINEL;
-    }
-
-    return init;
-  }
-
-  private static int fibonacciSumSlow(final int maxCount) {
-
+  private static int fibonacciSum(final Fibonacci implementation, final int maxCount) {
     int sum = 0;
     int count = maxCount;
 
     while (count > 0) {
       if (count % 2 == 0) {
-        //sum = fibonacciSlow(count);
-      }
-      count--;
-    }
-
-    return sum;
-  }
-
-  public static int fibonacciMemoized(int n) {
-    System.out.println("Memoized ");
-    if (n < 0) throw new IllegalArgumentException();
-
-    if (MEMOIZED[n] == INIT_SENTINEL) {
-      if (n < 2) MEMOIZED[n] = 1;
-      else MEMOIZED[n] = fibonacciSumMemoized(n-1) + fibonacciSumMemoized(n-2);
-    }
-
-    return MEMOIZED[n];
-  }
-
-  private static int fibonacciSumMemoized(final int maxCount) {
-
-    int sum = 0;
-    int count = maxCount;
-
-    while (count > 0) {
-      if (count % 2 == 0) {
-        sum = fibonacciMemoized(count);
+        sum = implementation.sequenceValue(count);
       }
       count--;
     }
@@ -62,10 +23,18 @@ public class Main {
 
   public static void main(String[] args) {
     Fibonacci bruteForceFibonacci = new BruteForceFibonacci();
-    List<Fibonacci> fibonaccis = Arrays.asList(bruteForceFibonacci);
+    Fibonacci memoizationFibonacci = new MemoizationFibonacci();
+    List<Fibonacci> fibonaccis = Arrays.asList(bruteForceFibonacci, memoizationFibonacci);
 
-    //System.out.println(fibonacciSlow(1));
-    System.out.println(fibonacciSumMemoized(MAX));
+    for (Fibonacci fibonacci : fibonaccis) {
+      try {
+        System.out.println(fibonacciSum(fibonacci, MAX));
+
+        // Catch the StackOverflow Error
+      } catch (Throwable t) {
+        System.out.println("Unable to generate " + t.getMessage());
+      }
+    }
   }
 
 }

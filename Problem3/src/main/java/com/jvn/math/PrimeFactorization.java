8e75de3e9679c96950d6e45d76e93d40ce29589a
John Van Note <johnlvannote@protonmail.com>
2020-02-08 21:16:12 -0500

    Finished the 3rd problem

---
+++ Problem3/src/main/java/com/jvn/math/PrimeFactorization.java
@@ -0,0 +1,29 @@
+package com.jvn.math;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorization {
+
+  private static final BigInteger NUMBER = new BigInteger("600851475143");
+
+  public static long largestPrimeFactor(BigInteger number) {
+    BigInteger numCopy = number;
+
+    List<Long> factors = new ArrayList<Long>();
+
+    for (long i=2; numCopy.compareTo(BigInteger.valueOf(i)) >= 0; i++) {
+      if (numCopy.mod(BigInteger.valueOf(i)).equals(BigInteger.ZERO)) {
+        factors.add(i);
+        numCopy = numCopy.divide(BigInteger.valueOf(i));
+      }
+    }
+    return factors.get(factors.size()-1);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(largestPrimeFactor(NUMBER));
+  }
+
+}

95d7aab2f180a604761e448a7630bfa10b010e44
John Van Note <johnlvannote@protonmail.com>
2020-02-08 23:00:59 -0500

    Problem 6 complete

---
+++ Problem6/src/main/java/com/jvn/euler/SumSquares.java
@@ -0,0 +1,31 @@
+package com.jvn.euler;
+
+public class SumSquares {
+
+  private static final int NUMBER = 100;
+
+  public static int sumOfSquares(int num) {
+    int total = 0;
+    for (int i=1; i<=num; i++) {
+      total += Math.pow(i, 2);
+    }
+    return total;
+  }
+
+  public static int squareOfSum(int num) {
+    int total = 0;
+    for (int i=1; i<=num; i++) {
+      total += i;
+    }
+    return new Double(Math.pow(total, 2)).intValue();
+  }
+
+  public static int difference(int num) {
+    return  squareOfSum(num) - sumOfSquares(num);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(difference(NUMBER));
+  }
+
+}
---
+++ Problem6/src/test/java/com/jvn/euler/SumSquares_TestCase.java
@@ -0,0 +1,13 @@
+package com.jvn.euler;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SumSquares_TestCase {
+
+  @Test
+  public void difference_10_2640() {
+    Assert.assertEquals(SumSquares.difference(10), 2640);
+  }
+
+}

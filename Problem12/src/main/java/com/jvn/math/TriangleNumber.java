00b154aa9684f973cc91f4f83cef1d1600216e76
John Van Note <johnlvannote@protonmail.com>
2020-03-08 20:01:17 -0400

    Updated with Problem 12

---
+++ Problem12/src/main/java/com/jvn/math/TriangleNumber.java
@@ -0,0 +1,42 @@
+package com.jvn.math;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TriangleNumber {
+
+  public static final int NUM_DIVISORS = 500;
+
+  public static long next(int num) {
+    return ((num + 1) * num) / 2;
+  }
+
+  public static Set<Long> getDivisors(long num) {
+    Set<Long> divisors = new HashSet<Long>();
+    divisors.add(1L);
+
+    for (long i = 1; i * i < num; i++) {
+      if (num % i == 0) {
+        divisors.add(i);
+        divisors.add(num / i);
+      }
+    }
+
+    return divisors;
+  }
+
+  public static void main(String[] args) {
+    int i = 1;
+    long next;
+    Set<Long> divisors;
+
+    do {
+      next = next(i);
+      divisors = getDivisors(next);
+      i++;
+    } while (divisors.size() < NUM_DIVISORS);
+
+    System.out.println(next);
+  }
+
+}
---
+++ Problem12/src/test/java/com/jvn/math/TriangleNumber_UnitTest.java
@@ -0,0 +1,47 @@
+package com.jvn.math;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TriangleNumber_UnitTest {
+
+  @DataProvider
+  public Object[][] sequences() {
+    return new Object[][] {
+        {1, 1},
+        {2, 3},
+        {3, 6},
+        {4, 10},
+        {5, 15},
+        {6, 21},
+        {7, 28}
+    };
+  }
+
+  @Test(dataProvider = "sequences")
+  public void next_sequences_expectedVal(int n, long expectedValue) {
+    Assert.assertEquals(TriangleNumber.next(n), expectedValue);
+  }
+
+  @DataProvider
+  public Object[][] divisors() {
+    return new Object[][] {
+        {1, ImmutableSet.of(1L)},
+        {3, ImmutableSet.of(1L, 3L)},
+        {6, ImmutableSet.of(1L, 2L, 3L, 6L)},
+        {10, ImmutableSet.of(1L, 2L, 5L, 10L)},
+        {15, ImmutableSet.of(1L, 3L, 5L, 15L)},
+        {21, ImmutableSet.of(1L, 3L, 7L, 21L)},
+        {28, ImmutableSet.of(1L, 2L, 4L, 7L, 14L, 28L)}
+    };
+  }
+
+  @Test(dataProvider = "divisors")
+  public void getDivisors_divisors_expectedVal(int n, Set<Long> expectedDivisors) {
+    Assert.assertEquals(TriangleNumber.getDivisors(n), expectedDivisors);
+  }
+
+}

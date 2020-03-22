454e6da81c85af80c45807ff6f0b36cef24d972e
John Van Note <johnlvannote@protonmail.com>
2020-03-22 14:20:44 -0400

    Updated with Problem 15 answers

---
+++ Problem15/src/main/java/com/jvn/math/LatticePaths.java
@@ -0,0 +1,40 @@
+package com.jvn.math;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LatticePaths {
+
+  private static final Map<List<Integer>, Long> memoization = new HashMap<List<Integer>, Long>();
+
+  public static long numRoutes(int x, int y) {
+
+    long routes = 0;
+    List<Integer> key = new ArrayList<Integer>(2);
+    key.add(x);
+    key.add(y);
+
+    if (!memoization.containsKey(key)) {
+      if (x == 0 && y ==0) {
+        routes++;
+      }
+      if (x > 0) {
+        routes += numRoutes(x - 1, y);
+      }
+      if (y > 0) {
+        routes += numRoutes(x, y - 1);
+      }
+
+      memoization.put(key, routes);
+    }
+
+    return memoization.get(key);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(numRoutes(20, 20));
+  }
+
+}
---
+++ Problem15/src/test/java/com/jvn/math/LatticePaths_UnitTest.java
@@ -0,0 +1,25 @@
+package com.jvn.math;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LatticePaths_UnitTest {
+
+  @DataProvider
+  public Object[][] sizes() {
+    return new Object[][] {
+        {0, 0, 1},
+        {1, 0, 1},
+        {1, 1, 2},
+        {1, 2, 3},
+        {2, 2, 6}
+    };
+  }
+
+  @Test(dataProvider = "sizes")
+  public void numRoutes_sizes_expectedValues(int x, int y, int expectedValue) {
+    Assert.assertEquals(LatticePaths.numRoutes(x, y), expectedValue);
+  }
+
+}

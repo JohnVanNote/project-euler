26acbf9b9aa0900f174efe398e249f6f238f0bfd
John Van Note <johnlvannote@protonmail.com>
2020-04-19 12:20:22 -0400

    Made the solution for problem 23 much more efficient

--- Problem23/src/main/java/com/jvn/math/AbundantNumber.java
+++ Problem23/src/main/java/com/jvn/math/AbundantNumber.java
@@ -1,7 +1,10 @@
 package com.jvn.math;
 
 import com.jvn.util.SetUtil;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AbundantNumber {
@@ -23,13 +26,26 @@ public class AbundantNumber {
     return abundants;
   }
 
+  public static boolean isSumOfTwoNumbers(Set<Integer> numbers, int sum) {
+    boolean isSum = false;
+    for (int number : numbers) {
+      int check = sum - number;
+      isSum = isSum || numbers.contains(check);
+      if (isSum) {
+        break;
+      }
+    }
+    return isSum;
+  }
+
   public static void main(String[] args) {
     Set<Integer> abundants = getAbundantUnder(UNDER);
-    Set<Integer> sumAbundants = SetUtil.sumAllSetItems(abundants);
+    List<Integer> sortedAbundants = new ArrayList<>(abundants);
+    Collections.sort(sortedAbundants);
 
     int sum = 0;
     for (int i = 1; i < UNDER; i++) {
-      if (!sumAbundants.contains(i)) {
+      if (!isSumOfTwoNumbers(abundants, i)) {
         sum += i;
       }
     }
--- util/src/main/java/com/jvn/util/SetUtil.java
+++ util/src/main/java/com/jvn/util/SetUtil.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
 
 public class SetUtil {
 
@@ -22,8 +21,8 @@ public class SetUtil {
     if (set.size() > 0) {
       List<Integer> list = new ArrayList<>(set);
       int firstItem = list.get(0);
-      for (int i = 0; i < list.size(); i++) {
-        newSet.add(firstItem + list.get(i));
+      for (int item : set) {
+        newSet.add(firstItem + item);
       }
       List<Integer> newList = list.subList(1, list.size());
       newSet.addAll(sumAllSetItems(new HashSet<>(newList)));

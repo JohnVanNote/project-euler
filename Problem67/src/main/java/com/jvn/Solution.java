ba30adadb24a9b72b8e8cd4c06afc20330d30723
John Van Note <johnlvannote@protonmail.com>
2020-12-30 10:08:25 -0500

    Finished 67 aince its basically 18

---
+++ Problem67/src/main/java/com/jvn/Solution.java
@@ -0,0 +1,28 @@
+package com.jvn;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Solution {
+
+  public static void main(String[] args) throws Exception {
+    //File f = new File(".");
+    //System.out.println(f.getAbsolutePath());
+    String triangleFile = "./Problem67/src/main/resources/triangle.txt";
+    BufferedReader reader = new BufferedReader(new FileReader(triangleFile));
+    StringBuilder content = new StringBuilder();
+    String line;
+    while ((line = reader.readLine()) != null) {
+      content.append(line);
+      content.append(System.getProperty("line.separator"));
+    }
+    reader.close();
+
+    String triangle = content.toString();
+
+    System.out.println(MaximumPathSum.findMaximumPath(MaximumPathSum.convertPlainTextToTreeMatrix(triangle)));
+
+  }
+
+}
